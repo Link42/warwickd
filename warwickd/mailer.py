3a1b9fa3ec2a1e6575046aad4cb9e382279056df
Heath Taylor <datahattrick@gmail.com>
2024-03-28 15:51:40 +1300

    simplified mailer class, removed config

--- warwickd/mailer.py
+++ warwickd/mailer.py
@@ -1,46 +1,45 @@
 import smtplib
 import logging
+from email.mime.multipart import MIMEMultipart
 from datetime import datetime
 from email.mime.text import MIMEText
 
 logger = logging.getLogger(__name__)
 
+
 class mailer:
+    def __init__(
+        self,
+        server: str,
+        port: int,
+        sender_name: str,
+        sender_address: str,
+        subject_extra: str,
+    ):
+        self.sender_name = sender_name
+        self.sender_address = sender_address
+        self.subject_extra = subject_extra
+        self.server = server
+        self.port = port
 
-    def __init__(self, config, subject, message):
-        self.config = config
-
-        # Email content
-        mail_content = "<html><body>" + message + "</body></html>"
-
-        # Add email headers
-        message = MIMEText(mail_content, "html")
-        message["From"] = (
-            self.config["mailer"]["from_name"]
-            + " <"
-            + self.config["mailer"]["from_address"]
-            + ">"
-        )
-        message["To"] = "<" + self.config["mailer"]["to_address"] + ">"
-        message["Subject"] = (
-            self.config["mailer"]["subject"]
+    def send_email(self, recipient_address: str, subject: str, body: str):
+        msg = MIMEMultipart()
+        msg["From"] = self.sender_name + " <" + self.sender_address + ">"
+        msg["To"] = "<" + recipient_address + ">"
+        msg["Subject"] = (
+            self.subject_extra
             + " "
             + subject
             + " - "
             + datetime.now().strftime("%a %H:%M:%S")
         )
+        msg = MIMEText("<html><body>" + body + "</body></html>", "html")
 
-        # Contect to SMTP server and send the mail ^_^
-        server = smtplib.SMTP(
-            self.config["mailer"]["smtp"]["server"]
-            + ":"
-            + str(self.config["mailer"]["smtp"]["port"])
-        )
-        server.sendmail(
-            self.config["mailer"]["from_address"],
-            self.config["mailer"]["to_address"],
-            message.as_string(),
-        )
-        server.quit()
+        try:
+            server = smtplib.SMTP(host=self.server,port=self.port)
+            server.sendmail(from_addr=self.sender_address, to_addrs=recipient_address, msg=msg.as_string())
+            server.quit()
+            logger.warning(f"Email sent for '{subject}'")
+        except Exception as e:
+            logger.error(f"Failed to send email. Error: {str(e)}")
 
-        logger.warning("Email sent for '" + subject + "'")
